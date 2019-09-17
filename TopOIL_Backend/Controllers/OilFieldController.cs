756d1f7212366695e3fc441f2092e4d035d6438d
Hafinator <maks.hafner@t-2.net>
2019-09-17 22:26:22 +0200

    Started working on CRUD

--- TopOIL_Backend/Controllers/OilFieldController.cs
+++ TopOIL_Backend/Controllers/OilFieldController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace TopOIL_Backend.Controllers
@@ -13,11 +14,31 @@ namespace TopOIL_Backend.Controllers
     {
         private int pageSize = 2;
 
+        private readonly Context _context;
+
+        public OilFieldController(Context context)
+        {
+            _context = context;
+        }
+
         // GET api/values
         [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
+        public ActionResult<IEnumerable<string>> Get([FromQuery(Name = "page")] string page)
         {
-            return new string[] { "value1", "value2" };
+            IActionResult result = BadRequest();
+            try
+            {
+                var oilField = await _context.OilFields.FindAsync(id);
+                _context.Remove(oilField);
+                result = Ok(true);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"EXCEPTION: {e.Message}");
+                result = StatusCode(StatusCodes.Status500InternalServerError);
+            }
+
+            return result;
         }
 
         // GET api/values/5
@@ -29,20 +50,50 @@ namespace TopOIL_Backend.Controllers
 
         // POST api/values
         [HttpPost]
-        public void Post([FromBody] string value)
+        public async Task<IActionResult> Post([FromBody] string value)
         {
+            IActionResult result = BadRequest();
+            return result;
         }
 
         // PUT api/values/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public async Task<IActionResult> Put(int id, [FromBody] string value)
         {
+            IActionResult result = BadRequest();
+            try
+            {
+                var oilField = await _context.OilFields.FindAsync(id);
+                _context.Remove(oilField);
+                result = Ok(true);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"EXCEPTION: {e.Message}");
+                result = StatusCode(StatusCodes.Status500InternalServerError);
+            }
+
+            return result;
         }
 
         // DELETE api/values/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public async Task<IActionResult> Delete(int id)
         {
+            IActionResult result = BadRequest();
+            try
+            {
+                var oilField = await _context.OilFields.FindAsync(id);
+                _context.Remove(oilField);
+                result = Ok(true);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"EXCEPTION: {e.Message}");
+                result = StatusCode(StatusCodes.Status500InternalServerError);
+            }
+
+            return result;
         }
     }
 }
